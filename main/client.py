a207113241e184d35aa8aeb109bdbc5371b33484
raymondispresent <79287938+raymondispresent@users.noreply.github.com>
2021-04-11 23:18:25 +0100

    adding server and client

---
+++ main/client.py
@@ -0,0 +1,26 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECTFROMTHESERVERLOL"
+SERVER = "192.168.0.66"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode('utf-8')
+    msg_length = len(message)
+    send_length = str(msg_length).encode('utf-8')
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode('utf-8'))
+
+send("Hello World!")
+input("start")
+
+
+send(DISCONNECT_MESSAGE)
