bf72615ed59a1693e524cee5393eb0d777b8d0f5
Raymond Yeung <hyeung@tcd.ie>
2021-04-13 15:32:49 +0100

    server fixes

--- main/client/client.py
+++ main/client/client.py
@@ -1,29 +1,29 @@
 import socket
+import csv
 import os
-
-SEPARATOR = "<SEPARATOR>"
-
-BUFFER_SIZE = 1024 * 4
-filename = "data.csv"
-host = "192.168.0.66"
 port = 5001
 
+host = "192.168.0.87"
+filename = "data.csv"
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
 
-filesize = os.path.getsize(filename)
 s = socket.socket()
 print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected.")
 
 def sendfile():
+    filesize = os.path.getsize(filename)
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
     with open(filename, "rb") as f:
         while True:
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
-                print("File done sending")
                 break
             s.sendall(bytes_read)
     s.close()
 
 sendfile()
+
---
+++ main/server/server.py
@@ -0,0 +1,36 @@
+import socket
+import os
+import csv
+
+# device's IP address
+SERVER_HOST = "192.168.0.87"
+SERVER_PORT = 5001
+
+# receive 4096 bytes each time
+BUFFER_SIZE = 4096
+
+SEPARATOR = "<SEPARATOR>"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen()
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+client_socket, address = s.accept() 
+print(f"[+] {address} is connected.")
+
+def receive_file():
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    filename = "filereceived.csv"
+    filemame = os.path.basename(filename)
+    filesize = int(filesize)
+    with open(filename, "wb") as f:
+        while True:
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            f.write(bytes_read)
+    client_socket.close()
+    s.close() 
+
+receive_file()
